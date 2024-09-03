2b10dfff1b15e0bdd3e8e234769a918c87b22140
PhilipeeX <dev.philipe@outlook.com>
2024-09-03 20:02:34 -0300

    Uses routing with outlet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ListRenderComponent} from "./components/list-render/list-render.component";
+import {FirstComponentComponent} from "./components/first-component/first-component.component";
+import {PipeComponent} from "./components/pipe/pipe.component";
+import {EventosComponent} from "./components/eventos/eventos.component";
+import {IfRenderComponent} from "./components/if-render/if-render.component";
+import {ChangeNumberComponent} from "./components/change-number/change-number.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: FirstComponentComponent},
+  { path: 'list', component: ListRenderComponent},
+  { path: 'pipe', component: PipeComponent},
+  { path: 'events', component: EventosComponent},
+  { path: 'render', component: IfRenderComponent},
+  { path: 'change-number', component: ChangeNumberComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
